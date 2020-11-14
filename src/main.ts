4d5f05ac54f98f8e4df9936597c307e6bd768824
vaaski <admin@vaa.ski>
2020-11-14 23:55:46 +0100

    feat(capacitor): add statusbar background color

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 
+import { Plugins, Capacitor } from "@capacitor/core"
+const { StatusBar } = Plugins
+
+if (Capacitor.isPluginAvailable("StatusBar"))
+  StatusBar.setBackgroundColor({
+    color: "#222436",
+  })
+
 Vue.config.productionTip = false
 
 new Vue({
